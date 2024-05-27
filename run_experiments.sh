24a07c742056a5d026d3bbe2524a417004639170
JuanKo96 <juanko@kaist.ac.kr>
2024-05-27 18:56:50 +0900

    change gamma to step_size

--- run_experiments.sh
+++ run_experiments.sh
@@ -13,14 +13,14 @@ pid_file="pids.txt"
 # Outer loop for 8 sets of seeds
 for outer in $(seq 0 0); do
     # Inner loop for 4 seeds in each set
-    for inner in $(seq 0 2); do
+    for inner in $(seq 0 0); do
         seed=$((outer * 3 + inner))
         
-        CUDA_VISIBLE_DEVICES=0 python main.py --config $config_file --seed $seed --model_type "DiagonalVariational" &
+        CUDA_VISIBLE_DEVICES=1 python main.py --config $config_file --seed $seed --model_type "DiagonalVariational" &
         echo $! >> $pid_file
-        CUDA_VISIBLE_DEVICES=1 python main.py --config $config_file --seed $seed --model_type "FullRankVariational" &
+        CUDA_VISIBLE_DEVICES=2 python main.py --config $config_file --seed $seed --model_type "FullRankVariational" &
         echo $! >> $pid_file
-        CUDA_VISIBLE_DEVICES=2 python main.py --config $config_file --seed $seed --model_type "StructuredVariational" &
+        CUDA_VISIBLE_DEVICES=3 python main.py --config $config_file --seed $seed --model_type "StructuredVariational" &
         echo $! >> $pid_file
     done
     # Wait for the current batch of 4 processes to finish before starting the next batch
